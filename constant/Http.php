cb94f1cff5fbf14cc4ab56ac4ef3b446a7bcd4f5
darkanorep <jeromeperona@yahoo.com>
2023-03-28 14:26:05 +0800

    constant

---
+++ constant/Http.php
@@ -0,0 +1,16 @@
+<?php
+
+    /*
+    define('NOT_FOUND', 404);
+    print NOT_FOUND;
+    */
+
+
+    class Http {
+
+        public const NOT_FOUND = 404;
+
+    }
+
+
+?>
---
+++ constant/constant.php
@@ -0,0 +1,7 @@
+<?php
+
+    require_once 'Http.php';
+
+    print Http::NOT_FOUND;
+
+?>
